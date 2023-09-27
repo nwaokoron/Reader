932ff225328a8d63791f086f41b70ad70e7ce0ec
Nzechi Nwaokoro <nwaokoron@gmail.com>
2023-09-27 14:42:15 -0700

    add user input for text to speech

--- synthesize.py
+++ synthesize.py
@@ -1,31 +1,41 @@
 #Synthesizes speech from the input string of text
 from google.cloud import texttospeech
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
+def textToSpeech(input_text):        
+ 
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=input_text)
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    # voice = texttospeech.VoiceSelectionParams(
+    #     language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+    # )
+
+    voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", name="en-US-Wavenet-B", ssml_gender=texttospeech.SsmlVoiceGender.MALE
 )
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+    )
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
 
-# The response's audio_content is binary.
-with open("output.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+
+user_input = input("what would you like to say: ")
+textToSpeech(user_input)
